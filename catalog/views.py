8da2a3f50ea695be65a0563454cf20cd5d7bf00d
Olga Aukhadieva <sarole4ka@gmail.com>
2024-04-10 11:22:02 +0500

    Fix contacts func (add POST handler)

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 
 
@@ -6,4 +8,11 @@ def index(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contacts_dict = {'contacts_data': [{'name': name, 'email': email, 'message': message}]}
+        with open('contacts_data.json', 'w') as j_file:
+            json.dump(contacts_dict, j_file, indent=4, ensure_ascii=False)
     return render(request, 'catalog/contacts.html')
