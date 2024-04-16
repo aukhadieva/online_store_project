d807a5e04410a15d2a1674357b89771d4f26e0b7
Olga Aukhadieva <sarole4ka@gmail.com>
2024-04-16 08:36:46 +0500

    Add models (Category and Product), made migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'prod_desc', 'category', 'price', 'created_at', 'updated_at',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'cat_desc',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='наименование категории')
+    cat_desc = models.TextField(max_length=500, verbose_name='описание категории')
+
+    def __str__(self):
+        return f'{self.category_name} {self.cat_desc}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='наименование')
+    prod_desc = models.TextField(max_length=500, verbose_name='описание продукта')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    in_stock = models.BooleanField(default=True, verbose_name='в наличии')
+
+    def __str__(self):
+        return (f'{self.product_name} {self.price} {self.prod_desc} {self.price} '
+                f'{self.created_at} {self.updated_at}')
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('in_stock',)
