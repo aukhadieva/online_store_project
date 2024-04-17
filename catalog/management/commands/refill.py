97e74ca942ed6656d8c103155fa2d99f6330192f
Olga Aukhadieva <sarole4ka@gmail.com>
2024-04-17 11:47:19 +0500

    Add custom command

---
+++ catalog/management/commands/refill.py
@@ -0,0 +1,52 @@
+import json
+
+from django.core.management import BaseCommand
+
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+
+    @staticmethod
+    def json_read_categories():
+        """
+        Получает данные из фикстур с категориями.
+        """
+        with open('category.json') as j_file:
+            load_categories = json.load(j_file)
+            commands_list = []
+            for item in load_categories:
+                commands_list.append(item['fields'])
+            return commands_list
+
+    @staticmethod
+    def json_read_products():
+        """
+        Получает данные из фикстур с продуктами.
+        """
+        with open('product.json') as j_file:
+            load_products = json.load(j_file)
+            commands_list = []
+            for item in load_products:
+                commands_list.append(item['fields'])
+            return commands_list
+
+    def handle(self, *args, **options):
+        """
+        Заполняет данные в базу данных, предварительно зачистив ее от старых данных.
+        """
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
+        categories_for_create = []
+        product_for_create = []
+
+        for categories in Command.json_read_categories():
+            categories_for_create.append(Category(**categories))
+
+        Category.objects.bulk_create(categories_for_create)
+
+        for products in Command.json_read_products():
+            product_for_create.append(Product(**products))
+
+        Product.objects.bulk_create(product_for_create)
