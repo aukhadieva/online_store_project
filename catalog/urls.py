26c04a1a2fe9c6ffc253bf4174caeac57e4f13a5
Olga Aukhadieva <sarole4ka@gmail.com>
2024-04-20 17:55:11 +0500

    Add app_name

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from catalog.views import index, contacts
 
 
+app_name = 'catalog'
+
 urlpatterns = [
     path('', index, name='home'),
     path('contacts/', contacts, name='contacts')
