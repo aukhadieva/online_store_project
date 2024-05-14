79bf1916603782e2d4bbecdefd4cd50a8bdd1016
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-14 11:55:51 +0500

    Add titles

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,19 @@ from django.views.generic import DetailView, ListView, CreateView, UpdateView, D
 
 from blog.models import BlogPost
 from config import settings
+from utils import TitleMixin
 
 
-class BlogPostCreateView(CreateView):
+class BlogPostCreateView(TitleMixin, CreateView):
     model = BlogPost
     fields = ('title', 'body', 'img_preview',)
     success_url = reverse_lazy('blog:posts')
+    title = 'Форма'
 
 
-class BlogPostListView(ListView):
+class BlogPostListView(TitleMixin, ListView):
     model = BlogPost
+    title = 'Блог'
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset(*args, **kwargs)
@@ -21,10 +24,13 @@ class BlogPostListView(ListView):
         return queryset
 
 
-class BlogPostDetailView(DetailView):
+class BlogPostDetailView(TitleMixin, DetailView):
     model = BlogPost
     slug_field = 'slug'
     slug_url_kwarg = 'slug'
+
+    def get_title(self):
+        return self.object.title
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
@@ -34,20 +40,22 @@ class BlogPostDetailView(DetailView):
             send_mail('Поздравляем!',
                       'Ваш пост набрал 100 просмотров!',
                       settings.EMAIL_HOST_USER,
-                      ['sarole4ka@gmail.com', 'saratova.olga.s@mail.ru'],
+                      ['saratova.olga.s@mail.ru'],
                       fail_silently=False, )
         return self.object
 
 
-class BlogPostUpdateView(UpdateView):
+class BlogPostUpdateView(TitleMixin, UpdateView):
     model = BlogPost
     fields = ('title', 'body', 'img_preview',)
+    title = 'Редактирование поста'
 
     def get_success_url(self):
         blog_post = self.get_object()
         return reverse('blog:view_post', args=[blog_post.slug])
 
 
-class BlogPostDeleteView(DeleteView):
+class BlogPostDeleteView(TitleMixin, DeleteView):
     model = BlogPost
     success_url = reverse_lazy('blog:posts')
+    title = 'Удаление поста'
