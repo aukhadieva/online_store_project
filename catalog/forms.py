bdffed17b054138f5d7507a663d3341185aa6efc
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-15 21:36:11 +0500

    Add VersionForm and update clean-methods for ProductForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
 class StyleMixin:
@@ -10,7 +10,7 @@ class StyleMixin:
             field.widget.attrs['class'] = 'form-control'
 
 
-class ProductModelForm(StyleMixin, forms.ModelForm):
+class ProductForm(StyleMixin, forms.ModelForm):
     BANNED_LIST = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
     class Meta:
@@ -19,14 +19,36 @@ class ProductModelForm(StyleMixin, forms.ModelForm):
 
     def clean_product_name(self):
         cleaned_data = self.cleaned_data['product_name']
-        if cleaned_data in self.BANNED_LIST:
-            raise forms.ValidationError('Название товара содержат недопустимые слова')
+        for word in cleaned_data.split():
+            if word in self.BANNED_LIST:
+                raise forms.ValidationError('Название товара содержат недопустимые слова')
 
         return cleaned_data
 
     def clean_prod_desc(self):
         cleaned_data = self.cleaned_data['prod_desc']
-        if cleaned_data in self.BANNED_LIST:
-            raise forms.ValidationError('Описание товара содержат недопустимые слова')
+        for word in cleaned_data.split():
+            if word in self.BANNED_LIST:
+                raise forms.ValidationError('Описание товара содержат недопустимые слова')
+
+        return cleaned_data
+
+
+class VersionForm(forms.ModelForm):
+    version_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label='Название версии')
+    version_number = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}), label='Номер версии')
+    is_current = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}), label='Признак текущей версии', required=False)
+
+    class Meta:
+        model = Version
+        fields = ('version_name', 'version_number', 'product', 'is_current')
+
+    def clean_is_current(self):
+        cleaned_data = self.cleaned_data['is_current']
+        versions = Version.objects.filter(is_current=True)
+        for version in versions:
+            if version.is_current:
+                version.is_current = False
+                version.save()
 
         return cleaned_data
