fc19ffc8056a3c0648a505db03ea23560cf75253
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-17 14:22:07 +0500

    Fix get_context_data (ProductListView)

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,9 @@ from utils import TitleMixin
 
 
 def index(request):
+    """
+    Главная страница с продуктами.
+    """
     context = {'title': 'Главная',
                'object_list': Product.objects.all()[:3]}
     return render(request, 'catalog/index.html', context)
@@ -28,10 +31,16 @@ class ProductUpdateView(TitleMixin, UpdateView):
     title = 'Редактирование продукта'
 
     def get_success_url(self):
+        """
+        Возвращает URL, на который должен быть перенаправлен пользователь после успешной обработки формы.
+        """
         product = self.get_object()
         return reverse_lazy('catalog:view_product', args=[product.pk])
 
     def get_context_data(self, **kwargs):
+        """
+        Добавляет в контекст форму с версиями продукта.
+        """
         context = super().get_context_data(**kwargs)
         VersionFormset = inlineformset_factory(Product, Version, form=VersionForm, extra=1)
         if self.request.method == 'POST' and Version:
@@ -41,6 +50,9 @@ class ProductUpdateView(TitleMixin, UpdateView):
         return context
 
     def form_valid(self, form):
+        """
+        Проверяет валидность формы и сохраняет ее.
+        """
         formset = self.get_context_data()['formset']
         self.object = form.save()
         if formset.is_valid():
@@ -59,6 +71,9 @@ class ProductDetailView(TitleMixin, DetailView):
     model = Product
 
     def get_title(self):
+        """
+        Возвращает заголовок страницы.
+        """
         return self.object.product_name
 
 
@@ -69,26 +84,33 @@ class ProductListView(TitleMixin, ListView):
     title = 'Каталог'
 
     def get_queryset(self, *args, **kwargs):
+        """
+        Возвращает список продуктов, отфильтрованных по категории (при применении фильтра).
+        """
         queryset = super().get_queryset(*args, **kwargs)
         category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id) if category_id else queryset
 
     def get_context_data(self, *args, **kwargs):
+        """
+        Добавляет в контекст список категорий и переопределяет список продуктов.
+        """
+        # получаем текущий контекст
         context_data = super().get_context_data(*args, **kwargs)
+        # добавляем категории в контекст
         context_data['categories'] = Category.objects.all()
+
+        # Получаем текущий список продуктов из `context_data`
+        products = context_data.get('object_list')
+        # для каждого продукта ищем текущую версию (`is_current=True`)
+        # и сохраняем ее в новое поле `current_version` у объекта продукта.
+        # Это поле добавляется динамически и не сохраняется в базе данных.
+        for product in products:
+            current_version = product.version.filter(is_current=True).first()
+            if current_version:
+                product.current_version = current_version
+        # возвращаем обновленный контекст
         return context_data
-
-        # вариант 1 - выводит только один товар (чья версия была изменена на активную)
-        # versions = Version.objects.filter(is_current=True)
-        # for version in versions:
-        #     products = Product.objects.filter(version=version)
-        #     context_data['object_list'] = products.all()
-        # return context_data
-
-        # вариант 2 - не работает
-        # for product in context_data.get('object_list'):
-        #     product.version = product.version.filter(is_current=True).first()
-        # return context_data
 
 
 class ContactTemplateView(TitleMixin, TemplateView):
@@ -96,6 +118,9 @@ class ContactTemplateView(TitleMixin, TemplateView):
     title = 'Обратная связь'
 
     def post(self, request):
+        """
+        Обрабатывает отправку формы обратной связи.
+        """
         if self.request.method == 'POST':
             name = self.request.POST.get('name')
             email = self.request.POST.get('email')
