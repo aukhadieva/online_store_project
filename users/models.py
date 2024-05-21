106da2fdebe2eeac0cc6e267c4c7d2df66ba1f07
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 19:47:19 +0500

    Add EmailVerification model

--- users/models.py
+++ users/models.py
@@ -11,6 +11,20 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
     country = models.CharField(max_length=70, verbose_name='страна', **NULLABLE)
+    is_verified_email = models.BooleanField(default=False, verbose_name='статус подтверждения эл. почты')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+class EmailVerification(models.Model):
+    key = models.UUIDField(unique=True, verbose_name='ключ')
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, verbose_name='пользователь')
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name='дата генерации ключа')
+    expiration = models.DateTimeField(verbose_name='срок действия ключа')
+
+    def __str__(self):
+        return f'Email verification for {self.user}'
+
+    class Meta:
+        verbose_name = 'проверка электронной почты'
