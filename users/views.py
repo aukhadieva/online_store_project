9dcc79516ad0cb5388234a4188e8c9ca8c5126b2
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 11:07:11 +0500

    Add CBV UserRegisterView

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegisterForm
 from users.models import User
 
 
@@ -9,3 +10,10 @@ class UserLoginView(LoginView):
     model = User
     form_class = UserLoginForm
     template_name = 'users/login.html'
+
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
