d97d9a929953286758d24506dab589a88e7c712c
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 19:55:25 +0500

    Register EmailVerification model

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, EmailVerification
 
 
 admin.site.register(User)
+
+
+@admin.register(EmailVerification)
+class AdminEmailVerification(admin.ModelAdmin):
+    list_display = ('id', 'user', 'key', 'created_at', 'expiration',)
+    list_filter = ('expiration',)
