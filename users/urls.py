685f1af6cdc94c6ec8d2900036003beb0222eb48
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 20:52:41 +0500

    Add url for CBV EmailVerificationView

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserRegisterView, UserProfileView
+from users.views import UserLoginView, UserRegisterView, UserProfileView, EmailVerificationView
 
 app_name = UsersConfig.name
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('profile/', UserProfileView.as_view(), name='profile')
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('verify/<str:email>/<uuid:key>', EmailVerificationView.as_view(), name='verify')
 ]
