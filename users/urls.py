8b79a591de4235a7ad53a6e84d42aef596eb2f86
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 17:42:24 +0500

    Add url for CBV UserProfileView

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,13 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserRegisterView
+from users.views import UserLoginView, UserRegisterView, UserProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', UserRegisterView.as_view(), name='register')
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile')
 ]
