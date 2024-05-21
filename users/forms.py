c8b70029644516732852349c476fd7ef6c349e02
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 17:38:55 +0500

    Add UserProfileForm

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
@@ -22,3 +23,17 @@ class UserRegisterForm(StyleMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2',)
+
+
+class UserProfileForm(StyleMixin, UserChangeForm):
+    """
+    Форма для редактирования профиля пользователя.
+    """
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'phone', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.HiddenInput()
