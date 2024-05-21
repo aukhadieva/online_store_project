763fd346f2a08280b1bb5fe3b3b90f0c2e768776
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-21 19:01:14 +0500

    Add title

--- users/views.py
+++ users/views.py
@@ -4,25 +4,29 @@ from django.views.generic import CreateView, UpdateView
 
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from users.models import User
+from utils import TitleMixin
 
 
-class UserLoginView(LoginView):
+class UserLoginView(TitleMixin, LoginView):
     model = User
     form_class = UserLoginForm
     template_name = 'users/login.html'
+    title = 'Авторизация'
 
 
-class UserRegisterView(CreateView):
+class UserRegisterView(TitleMixin, CreateView):
     model = User
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+    title = 'Регистрация'
 
 
-class UserProfileView(UpdateView):
+class UserProfileView(TitleMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     success_url = reverse_lazy('users:profile')
+    title = 'Профиль'
 
     def get_object(self, queryset=None):
         """
