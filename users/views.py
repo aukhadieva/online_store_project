762957ed61dbbf86731de8feb96ae037ef967424
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-22 14:32:42 +0500

    Add CBV UserResetPasswordView

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
+import secrets
+
+from django.conf import settings
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.views import LoginView
-from django.shortcuts import HttpResponseRedirect
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.shortcuts import HttpResponseRedirect, render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, UpdateView, TemplateView
 
+from config.settings import DOMAIN_NAME
 from users.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from users.models import User, EmailVerification
 from utils import TitleMixin
@@ -55,3 +63,36 @@ class EmailVerificationView(TitleMixin, TemplateView):
             return super().get(request, *args, **kwargs)
         else:
             return HttpResponseRedirect(reverse('catalog:home'))
+
+
+class UserResetPasswordView(TitleMixin, TemplateView):
+    title = 'Восстановление пароля'
+    template_name = 'users/pwd_reset.html'
+
+    def post(self, request):
+        """
+        Обрабатывает запрос на восстановление пароля.
+        """
+        if self.request.method == 'POST':
+            email = self.request.POST.get('email')
+            try:
+                user = User.objects.get(email=email)
+                token = secrets.token_hex(10)
+                link = f'{DOMAIN_NAME}/users'
+                if User.objects.filter(email=user.email).exists():
+                    subject = f'Восстановление пароля'
+                    message = (f'Для восстановления доступа к личному кабинет пройдите по ссылке: {link} '
+                               f'и воспользуйтесь временным паролем:\n {token}')
+                    send_mail(
+                        subject=subject,
+                        message=message,
+                        from_email=settings.EMAIL_HOST_USER,
+                        recipient_list=[user.email],
+                        fail_silently=False,
+                    )
+                    user.password = make_password(token, salt=None, hasher="default")
+                    user.save()
+            except User.DoesNotExist:
+                # return HttpResponse(f'Пользователь с электронной почтой {email} не найден.')
+                return HttpResponseRedirect(reverse('users:login'))
+        return render(request, self.template_name)
