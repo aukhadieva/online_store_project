c59a1fef158ca96a18a638834068db5b47316e46
Olga Aukhadieva <sarole4ka@gmail.com>
2024-05-22 14:32:58 +0500

    Add url for CBV UserResetPasswordView

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,8 @@ from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserLoginView, UserRegisterView, UserProfileView, EmailVerificationView
+from users.views import UserLoginView, UserRegisterView, UserProfileView, EmailVerificationView, UserResetPasswordView
+
 
 app_name = UsersConfig.name
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('verify/<str:email>/<uuid:key>', EmailVerificationView.as_view(), name='verify')
+    path('verify/<str:email>/<uuid:key>', EmailVerificationView.as_view(), name='verify'),
+    path('pwd_reset/', UserResetPasswordView.as_view(), name='pwd_reset')
 ]
